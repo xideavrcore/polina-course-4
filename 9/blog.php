e6ef71d15d3364f75dfa419161a528e0d51ebe80
Red Fox <redfox@MacBook-Pro-Red.local>
2026-09-15 14:30:35 +0700

    lab9 start

---
+++ 9/blog.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>MyTravelNotes - блог</title>
+</head>
+<body>
+    <div style="border: 1px solid #999; padding: 6px;">
+        <a href="#">Войти</a> |
+        <a href="newnote.php">Новая запись</a> |
+        <a href="email.php">Отправить сообщение</a> |
+        <a href="#">Фото</a> |
+        <a href="#">Файлы</a> |
+        <a href="#">Администратору</a> |
+        <a href="inform.php">Информация</a> |
+        <a href="#">Выйти</a>
+    </div>
+    <p><em>Рад приветствовать вас<br>
+    на страницах моего сайта, посвященного путешествиям!<br>
+    Здесь я буду рассказывать о своих путешествиях...<br>
+    .... и выкладывать разные интересные материалы!</em></p>
+
+<form method="get" action="blog.php">
+Поиск:
+<input type="text" name="usersearch" size="40"
+value="<?php if (!empty($_GET['usersearch'])) echo $_GET['usersearch']; ?>" />
+<input type="submit" value="Найти" />
+</form>
+<br>
+
+<?php require_once ("connections/MySiteDB.php"); ?>
+
+<?php
+mysqli_select_db($link, $db);
+
+$user_search = '';
+if (isset($_GET['usersearch'])) {
+    $user_search = $_GET['usersearch'];
+}
+
+if (!empty($user_search)) {
+    //Поиск по фразе (по содержанию заметки)
+    $where_list = array();
+    $query_usersearch = "SELECT * FROM notes";
+    $clean_search = str_replace(',', ' ', $user_search);
+    $search_words = explode(' ', $clean_search);
+    //Создаем еще один массив с окончательными результатами
+    $final_search_words = array();
+    //Проходим в цикле по каждому элементу массива $search_words.
+    //Каждый непустой элемент добавляем в массив $final_search_words
+    if (count($search_words) > 0) {
+        foreach ($search_words as $word) {
+            if (!empty($word)) {
+                $final_search_words[] = $word;
+            }
+        }
+    }
+    //работа с использованием массива $final_search_words
+    foreach ($final_search_words as $word) {
+        $where_list[] = " article LIKE '%$word%'";
+    }
+    $where_clause = implode(' OR ', $where_list);
+    if (!empty($where_clause)) {
+        $query_usersearch .= " WHERE $where_clause";
+    }
+    $res_query = mysqli_query($link, $query_usersearch);
+    while ($res_array = mysqli_fetch_array($res_query)) {
+        echo $res_array['id'], "<br>";
+?>
+<a href="comments.php?note=<?php echo $res_array['id']; ?>">
+<?php echo $res_array['title'], "<br>"; ?></a>
+<?php
+        echo $res_array['article'], "<br>", "<hr>", "<br>";
+    }
+} else {
+    $query = "SELECT * FROM notes ORDER BY id DESC";
+    $select_note = mysqli_query($link, $query);
+
+    while ($note = mysqli_fetch_array($select_note)) {
+        echo $note['id'], "<br>";
+?>
+<a href="comments.php?note=<?php echo $note['id']; ?>">
+<?php echo $note['title'], "<br>"; ?></a>
+<?php
+        echo $note['created'], "<br>";
+        echo $note['article'], "<br>";
+    }
+}
+?>
+</body>
+</html>
---
+++ 9/create_other_table.php
@@ -0,0 +1,89 @@
+<?php
+mysqli_report(MYSQLI_REPORT_OFF);
+
+//Соединение с сервером
+$link = mysqli_connect(getenv('MYSQL_HOST') ?: 'localhost', 'admin', 'admin');
+
+//Выбор БД
+$db = "MySiteDB";
+$select = mysqli_select_db($link, $db);
+if ($select) {
+    echo "  База успешно выбрана", "<br>";
+} else {
+    echo "База не выбрана", "<br>";
+}
+
+//Создание таблицы comments (упр. 4)
+$query = "CREATE TABLE comments
+(id SMALLINT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (id),
+  created DATE NOT NULL,
+  author VARCHAR (20) NOT NULL,
+  comment VARCHAR (255) NOT NULL,
+  art_id INT NOT NULL)";
+
+$create_tbl = mysqli_query($link, $query);
+if ($create_tbl) {
+    echo " Таблица comments успешно создана", "<br>";
+} else {
+    echo "Таблица comments не создана", "<br>";
+}
+
+//Связь comments.art_id -> notes.id (упр. 5)
+$query = "ALTER TABLE comments
+  ADD CONSTRAINT fk_comments_notes
+  FOREIGN KEY (art_id) REFERENCES notes(id)";
+
+$fk = mysqli_query($link, $query);
+if ($fk) {
+    echo " Связь между таблицами создана", "<br>";
+} else {
+    echo "Связь не создана", "<br>";
+}
+
+//Тестовые записи (упр. 5, Insert)
+$notes_count = 0;
+$res = mysqli_query($link, "SELECT COUNT(*) AS c FROM notes");
+if ($res) {
+    $row = mysqli_fetch_array($res);
+    $notes_count = (int)$row['c'];
+}
+
+if ($notes_count == 0) {
+    $ok = mysqli_query($link, "INSERT INTO notes (created, title, article) VALUES
+('2026-03-01', 'Paris', 'First trip to Paris in spring.'),
+('2026-03-10', 'Rome', 'Colosseum and good coffee.'),
+('2026-04-05', 'Berlin', 'Museums and parks.')");
+    if ($ok) {
+        echo " Записи в notes добавлены", "<br>";
+    } else {
+        echo "Записи в notes не добавлены", "<br>";
+    }
+} else {
+    echo " В notes уже есть записи, пропуск вставки", "<br>";
+}
+
+$comments_count = 0;
+$res = mysqli_query($link, "SELECT COUNT(*) AS c FROM comments");
+if ($res) {
+    $row = mysqli_fetch_array($res);
+    $comments_count = (int)$row['c'];
+}
+
+if ($comments_count == 0) {
+    $ok = mysqli_query($link, "INSERT INTO comments (created, author, comment, art_id) VALUES
+('2026-03-02', 'Anna', 'Very interesting!', 1),
+('2026-03-11', 'Igor', 'Want to visit Rome', 2),
+('2026-03-12', 'Maria', 'Thanks for the story', 2),
+('2026-04-06', 'Petr', 'Looking forward', 3)");
+    if ($ok) {
+        echo " Записи в comments добавлены", "<br>";
+    } else {
+        echo "Записи в comments не добавлены", "<br>";
+    }
+} else {
+    echo " В comments уже есть записи, пропуск вставки", "<br>";
+}
+
+echo "<br>ВНИМАНИЕ, ПОСЛЕ ВЫПОЛНЕНИЯ ЭТОГО СКРИПТА ПЕРЕД СДАЧЕЙ ЛАБЫ НУЖНО ЕГО УДАЛИТЬ!!!!";
+?>
